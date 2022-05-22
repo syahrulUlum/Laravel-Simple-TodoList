5e5dc412cf8c09383dfe51e43b325b8d4fe1811a
syahrulUlum <sahrululum73@gmail.com>
2022-05-23 02:35:22 +0700

    Update View With Bootstrap

--- resources/views/layouts/list.blade.php
+++ resources/views/layouts/list.blade.php
@@ -10,23 +10,24 @@
                     <div class="ml-2">
                         <h6 class="mb-0">{{ $todo->title }}</h6>
                         <div class="d-flex flex-row mt-1 text-black-50 date-time">
-                            <div><i class="fa fa-calendar-o"></i><span class="ml-2">{{ $todo->date }}</span>
+                            <div><i class="fa fa-calendar-o"></i><span
+                                    class="ml-2">{{ date('d M Y', strtotime($todo->date)) }}</span>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="d-flex flex-row align-items-center">
-                    <div>
+                    <div class="d-inline-flex ">
                         @if (!$todo->is_done)
                             <form action="{{ route('todo.done', $todo->id) }}" method="POST">
                                 @csrf
                                 @method('PUT')
-                                <button class="btn btn-success" type="submit"
+                                <button class="btn btn-success mr-1" type="submit"
                                     onclick="return confirm('Done ?')">Done</button>
                             </form>
-                            <a href="{{ route('todo.edit', $todo->id) }}" class="btn btn-primary">Edit</a>
+                            <a href="{{ route('todo.edit', $todo->id) }}" class="btn btn-primary mr-1">Edit</a>
                         @endif
-                        <form action="{{ route('todo.destroy', $todo->id) }}" method="POST" class="form-inline">
+                        <form action="{{ route('todo.destroy', $todo->id) }}" method="POST">
                             @csrf
                             @method('DELETE')
                             <button class="btn btn-danger" type="submit" onclick="return confirm('Delete ?')">Delete
